36b0de08879ada6014fc87f39648bfbbc3a4f489
kiiittt <117594092+kiiittt@users.noreply.github.com>
2023-04-08 19:01:37 +0000

    Deploying to gh-pages from @ kiiittt/goit-js-hw-09@1d03dad5a8a28d25278e6139727c9e33f858983f 🚀

---
+++ 01-color-switcher.db985048.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.db985048.js.map
