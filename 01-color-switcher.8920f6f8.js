6882218d6b028920348931a48178ab31b712daf9
kiiittt <117594092+kiiittt@users.noreply.github.com>
2023-04-08 18:50:14 +0000

    Deploying to gh-pages from @ kiiittt/goit-js-hw-09@1cf4e2787a0e4d39c83255c1aee564dcc98399b4 🚀

---
+++ 01-color-switcher.8920f6f8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),e=null;t.addEventListener("click",(function(){e=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.8920f6f8.js.map
