1cf4e2787a0e4d39c83255c1aee564dcc98399b4
Darya <daryakolisnik@gmail.com>
2023-04-08 21:49:48 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+});
